e443ed9aac52c6a1a17e2b282d197156bfdf8a41
Ubuntu <azureuser@kube01.frao5mxcclaedmasaqkybkf3ma.ux.internal.cloudapp.net>
2026-02-25 22:20:32 +0000

    DAST testing

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ router.get('/search', (req, res) => {
   const query = req.query.q;
 
   // Vulnerable rendering
-  res.send( { query } );
+  res.send( `<h1>Search Results for: ${query}</h1>` );
 });
 
 router.get('/', (req, res) => {
